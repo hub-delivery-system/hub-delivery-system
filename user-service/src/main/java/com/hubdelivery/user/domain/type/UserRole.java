85fafbafe76efed74b5542812e1fc3f6676b27e7
sinyeowon <ioohyou@knu.ac.kr>
2026-05-22 11:12:40 +0900

    feat: p_users 엔티티와 사용자 권한/상태 타입 추가 - User 엔티티에 soft delete 필터/기본 상태(PENDING) 반영 - role/status/affiliation 타입 enum 정의

---
+++ user-service/src/main/java/com/hubdelivery/user/domain/type/UserRole.java
@@ -0,0 +1,8 @@
+package com.hubdelivery.user.domain.type;
+
+public enum UserRole {
+    MASTER,
+    HUB_MANAGER,
+    DELIVERY_MANAGER,
+    COMPANY_MANAGER
+}
---
+++ user-service/src/main/java/com/hubdelivery/user/domain/type/UserStatus.java
@@ -0,0 +1,7 @@
+package com.hubdelivery.user.domain.type;
+
+public enum UserStatus {
+    PENDING,
+    APPROVED,
+    REJECTED
+}
