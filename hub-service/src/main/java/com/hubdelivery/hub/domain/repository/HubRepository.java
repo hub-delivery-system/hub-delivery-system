12c0939f89c65c14e2d14f214e4f1653961833c5
Jieunbakk <jieun134@knu.ac.kr>
2026-05-26 00:22:53 +0900

    fix: HubRepository @Param import 수정

--- hub-service/src/main/java/com/hubdelivery/hub/domain/repository/HubRepository.java
+++ hub-service/src/main/java/com/hubdelivery/hub/domain/repository/HubRepository.java
@@ -1,11 +1,11 @@
 package com.hubdelivery.hub.domain.repository;
 
 import com.hubdelivery.hub.domain.entity.HubEntity;
-import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.math.BigDecimal;
 import java.util.Optional;
