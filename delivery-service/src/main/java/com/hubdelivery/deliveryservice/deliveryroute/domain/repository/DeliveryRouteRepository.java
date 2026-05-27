97f90e5587a6aa5e173ec6b8b47e47e94fd1c39d
wlsdlf2 <wlsdlfzg@naver.com>
2026-05-27 22:18:11 +0900

    feat: DeliveryRoute 검색 필터 구현

--- delivery-service/src/main/java/com/hubdelivery/deliveryservice/deliveryroute/domain/repository/DeliveryRouteRepository.java
+++ delivery-service/src/main/java/com/hubdelivery/deliveryservice/deliveryroute/domain/repository/DeliveryRouteRepository.java
@@ -10,7 +10,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-public interface DeliveryRouteRepository extends JpaRepository<DeliveryRoute, UUID> {
+public interface DeliveryRouteRepository extends JpaRepository<DeliveryRoute, UUID>, DeliveryRouteRepositoryCustom {
 
     Optional<DeliveryRoute> findByIdAndDeletedAtIsNull(UUID id);
 
