67b09e508a9a4c21c08c7694dbe8ae6a6f5db8d6
wlsdlf2 <wlsdlfzg@naver.com>
2026-05-27 12:53:44 +0900

    fix: GlobalExceptionHandler @Import 누락으로 인한 비즈니스 예외 500 반환 수정

--- delivery-service/src/main/java/com/hubdelivery/deliveryservice/DeliveryServiceApplication.java
+++ delivery-service/src/main/java/com/hubdelivery/deliveryservice/DeliveryServiceApplication.java
@@ -1,6 +1,7 @@
 package com.hubdelivery.deliveryservice;
 
 import com.hubdelivery.common.config.JpaAuditingConfig;
+import com.hubdelivery.common.exception.GlobalExceptionHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -8,7 +9,7 @@ import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @EnableFeignClients
-@Import(JpaAuditingConfig.class)
+@Import({JpaAuditingConfig.class, GlobalExceptionHandler.class})
 public class DeliveryServiceApplication {
 
 	public static void main (String[] args) {
