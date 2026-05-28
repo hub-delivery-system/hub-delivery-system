390e8571143094c494d37226e61379e1fa68a6e5
wlsdlf2 <wlsdlfzg@naver.com>
2026-05-29 00:50:06 +0900

    feat: delivery 목록 조회에 deliveryManagerId·date 필터 추가
    slack-service 스케줄러가 Feign으로 특정 담당자의 당일 배송 목록을 조회할 수 있도록
    DeliverySearchCondition에 deliveryManagerId, date 필드를 추가하고
    buildCondition에서 해당 조건을 반영하도록 구현.
    - deliveryManagerId: MASTER 권한에서만 적용 (DELIVERY_MANAGER는 X-User-Id 헤더 기반 자동 고정)
    - date: 지정일 00:00 이상 ~ 익일 00:00 미만 범위로 createdAt 필터링

--- delivery-service/src/main/java/com/hubdelivery/deliveryservice/delivery/presentation/dto/DeliverySearchCondition.java
+++ delivery-service/src/main/java/com/hubdelivery/deliveryservice/delivery/presentation/dto/DeliverySearchCondition.java
@@ -1,6 +1,7 @@
 package com.hubdelivery.deliveryservice.delivery.presentation.dto;
 
 import com.hubdelivery.deliveryservice.delivery.domain.type.DeliveryStatus;
+import java.time.LocalDate;
 import java.util.UUID;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -22,4 +23,18 @@ public class DeliverySearchCondition {
 
     /** 주문 ID 필터 (선택) */
     private UUID orderId;
+
+    /**
+     * 배송 담당자 ID 필터 (선택).
+     * MASTER 권한에서만 유효하며, DELIVERY_MANAGER 권한은 X-User-Id 헤더로 자동 고정되므로 이 값을 무시한다.
+     * 주 용도: slack-service 스케줄러가 Feign으로 특정 담당자의 배송 목록을 조회할 때 사용.
+     */
+    private UUID deliveryManagerId;
+
+    /**
+     * 생성일 기준 날짜 필터 (선택).
+     * 지정 시 해당 날짜 00:00:00 이상 ~ 익일 00:00:00 미만 범위로 조회한다.
+     * 주 용도: 스케줄러에서 당일 배송 목록만 추출할 때 사용.
+     */
+    private LocalDate date;
 }
