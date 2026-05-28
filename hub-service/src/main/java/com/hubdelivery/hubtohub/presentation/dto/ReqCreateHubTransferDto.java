67849569fde208b788c3d8253351b37276bc1128
Jieunbakk <jieun134@knu.ac.kr>
2026-05-28 16:03:39 +0900

    feat: Dto 추가

---
+++ hub-service/src/main/java/com/hubdelivery/hubtohub/presentation/dto/ReqCreateHubTransferDto.java
@@ -0,0 +1,28 @@
+package com.hubdelivery.hubtohub.presentation.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+/**
+ * Hub 이동 경로 생성 요청 DTO
+ */
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ReqCreateHubTransferDto {
+
+    /**
+     * 출발 허브 ID
+     */
+    private UUID fromHubId;
+
+    /**
+     * 도착 허브 ID
+     */
+    private UUID toHubId;
+}
